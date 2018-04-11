71926f3598db2c0393b2ad252265e9c88ea81441
mike <mixailshalin@yandex.ru>
2018-04-11 10:21:50 +0400

    master || move to app switch routing

--- source/js/components/App/App.js
+++ source/js/components/App/App.js
@@ -1,16 +1,37 @@
 import React,{Component} from 'react';
 import AuthComponent from 'js/components/Auth/';
-import PersonalArea from 'js/components/PersonalArea/';
+import {authRequest} from 'js/actions/Auth/AuthActions.js';
+import Table from 'js/components/Table/';
 import {connect} from "react-redux";
-import {withRouter} from 'react-router-dom';
+import {withRouter,Switch,Redirect,Route,Link} from 'react-router-dom';
 import {bankNameRequest} from 'js/actions/Bank/BankActions';
+import Transactions from 'js/components/Transactions/';
 
 export class App extends Component{
+    handleExit=(e)=>{
+        const {authRequest}=this.props;
+        e.preventDefault();
+        authRequest({
+            login:false,
+            password:false,
+        });
+    };
     render(){
         const {Auth}=this.props;
         return(
             <div>
-                {Auth?<PersonalArea/>:<AuthComponent/>}
+                {Auth?
+                <ul>
+                    <li><Link to="/" component="Table">Таблица транзакций</Link></li>
+                    <li><Link to="/Transactions" component="Transactions">Добавить транакцию</Link></li>
+                    <li><Link to="/exit" component="exit" onClick={this.handleExit}>Выход</Link></li>
+                </ul>:""
+                }
+                <Switch>
+                    <Route path="/" exact component={Auth?Table:AuthComponent}/>
+                    <Route path="/Transactions"  component={Transactions}/>
+                    <Redirect from="*" to="/"/>
+                </Switch>
             </div>
         )
     }
@@ -24,8 +45,8 @@ const mapStateToProps=(state)=>{
 
 const mapDispatchToProps=(dispatch)=>{
     return{
-        bankNameRequest:()=>{
-            dispatch(bankNameRequest());
+        authRequest:(logIn)=>{
+            dispatch(authRequest(logIn));
         }
     }
 };
